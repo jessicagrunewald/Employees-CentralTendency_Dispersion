60dcca3943b95ea085fece1b361bb177e2aa9728
jessicagrunewald <j_grunewald@hotmail.com>
2024-04-15 16:28:34 +1200

    Installation of packages and libraries for calculations and creation of graphs

---
+++ Employees.R
@@ -0,0 +1,9 @@
+# Instalação de pacotes e acesso a library que permite calcular a MODA
+install.packages("modeest")
+library(modeest)
+
+# Instalação de pacotes e acesso a library para criação de gráficos
+install.packages("ggplot2")
+install.packages("tidyverse")
+library(tidyverse)
+library(ggplot2)
