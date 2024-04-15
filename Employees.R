27330545d1555520441fb3a4d2cecf81cd65f1d3
jessicagrunewald <j_grunewald@hotmail.com>
2024-04-15 16:59:44 +1200

    Central tendency and dispersion measures

--- Employees.R
+++ Employees.R
@@ -1,9 +1,49 @@
-# Instalação de pacotes e acesso a library que permite calcular a MODA
+# Installing packages and accessing the library to calculate MODA
 install.packages("modeest")
 library(modeest)
 
-# Instalação de pacotes e acesso a library para criação de gráficos
+# Installing packages and accessing libraries to generate graphics
 install.packages("ggplot2")
 install.packages("tidyverse")
 library(tidyverse)
 library(ggplot2)
+
+# Importando a base de dados
+bd <- read.table("BD1.csv", header=TRUE, sep=";")
+
+# Central tendency and dispersion measures
+
+# Age
+cat("AGE 
+    Mean:", mean(bd$idade),
+    "Median:", median(bd$idade),
+    "Moda:", mlv(bd$idade),
+    "Standard Deviation:", sd(bd$idade),
+    "Variance:", sd(bd$idade) * sd(bd$idade))
+
+# Last salary amount
+cat("LAST SALARY VALUE 
+    Mean:", mean(bd$valor.do.último.salário),
+    "Median:", median(bd$valor.do.último.salário),
+    "Moda:", mlv(bd$valor.do.último.salário),
+    "Standard Deviation:", sd(bd$valor.do.último.salário),
+    "Variance:", sd(
+      bd$valor.do.último.salário) * sd(bd$valor.do.último.salário))
+
+# Length of professional experience
+cat("PROFESSIONAL EXPERIENCE LENGTH
+    Mean:", mean(bd$tempo.de.experiência.profissional),
+    "Median:", median(bd$tempo.de.experiência.profissional),
+    "Moda:", mlv(bd$tempo.de.experiência.profissional),
+    "Standard Deviation:", sd(bd$tempo.de.experiência.profissional),
+    "Variance:", sd(
+      bd$tempo.de.experiência.profissional) * sd(bd$tempo.de.experiência.profissional))
+
+# Length of experience in the role
+cat("TIME OF EXPERIENCE IN THE ROLE
+    Mean:", mean(bd$tempo.de.experiência.na.função),
+    "Median:", median(bd$tempo.de.experiência.na.função),
+    "Moda:", mlv(bd$tempo.de.experiência.na.função),
+    "Standard Deviation:", sd(bd$tempo.de.experiência.na.função),
+    "Variance:", sd(
+      bd$tempo.de.experiência.na.função) * sd(bd$tempo.de.experiência.na.função))
